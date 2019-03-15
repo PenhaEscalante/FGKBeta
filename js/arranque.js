12775676239930f180433791a0330ce963870a80
Eduardo Enrique Peña <pe12012@ues.edu.sv>
2019-03-14 19:50:11 -0600

    Beta

---
+++ js/arranque.js
@@ -0,0 +1,11 @@
+'use strict'
+
+$(document).ready(function(){
+
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    $('select').formSelect();
+
+});
+
