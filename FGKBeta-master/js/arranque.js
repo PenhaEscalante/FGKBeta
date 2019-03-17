0da008aec0cdbdd6714dfeecadcc0e4abb9a3eef
4lfred <34311627+4lfred@users.noreply.github.com>
2019-03-16 21:13:15 -0600

    Add files via upload

---
+++ FGKBeta-master/js/arranque.js
@@ -0,0 +1,13 @@
+'use strict'
+
+$(document).ready(function(){
+
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    $('.datepicker').datepicker();
+    $('select').formSelect();
+    $('.dropdown-trigger').dropdown();
+    $('.modal').modal();
+});
+
---
+++ FGKBeta-master/vistas/menu.php
@@ -0,0 +1,43 @@
+<ul id="slide-out" class="sidenav sidenav-fixed z-depth-3">
+    <li>
+        <div class="user-view encabezado-aside">
+            <div>
+                <img id="logo" src="../img/oportunidades.png">
+            </div>
+            <a href="#name"><span class="blue-grey-text text-lighten-1 name">Nombre Usuario</span></a>
+            <a href="#email"><span class="blue-grey-text text-lighten-1 email">Rol</span></a>
+        </div>
+    </li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect principal-item">Datos Generales<i
+                        class="material-icons">settings</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="departamento.php" class="waves-effect">Departamentos</a></li>
+                        <li><a href="municipio.php" class="waves-effect">Municipios</a></li>
+                        <li><a href="#!" class="waves-effect">Centros Escolares</a></li>
+                        <li><a href="#!" class="waves-effect">Personal</a></li>
+                        <li><a href="#!" class="waves-effect">Secciones</a></li>
+                        <li><a href="#!" class="waves-effect">Año en curso</a></li>
+                    </ul>
+                </div>
+            </li>
+
+            <li>
+                <a class="collapsible-header waves-effect principal-item">Alumnos<i
+                    class="material-icons">assignment_ind</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="alumno.php" class="waves-effect">Nuevo Alumno</a></li>
+                        <li><a href="detalle_alumno.php" class="waves-effect">Detalle de Alumno</a></li>
+                        
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>   
+
+</ul>
+<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
