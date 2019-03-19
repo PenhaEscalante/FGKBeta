0262696709f5cf0c5a7d53abf47240d67b1eed76
4lfred <alfzal74@gmail.com>
2019-03-18 20:20:53 -0600

    commit

--- vistas/menu.php
+++ vistas/menu.php
@@ -8,65 +8,39 @@
             <a href="#email"><span class="blue-grey-text text-lighten-1 email">Rol</span></a>
         </div>
     </li>
-    <li class="no-padding">
-        <ul class="collapsible collapsible-accordion">
-            <li>
-                <a class="collapsible-header waves-effect principal-item">Datos Generales<i
-                        class="material-icons">settings</i></a>
-                <div class="collapsible-body">
-                    <ul>
-                        <li><a href="departamento.php" class="waves-effect">Departamentos</a></li>
-                        <li><a href="municipio.php" class="waves-effect">Municipios</a></li>
-                        <li><a href="class.php" class="waves-effect">Classs</a></li>
-                        <li><a href="seccion.php" class="waves-effect">Seccion</a></li>
-                        <li><a href="school.php" class="waves-effect">Centros Educativos</a></li>
-                        <li><a href="#!" class="waves-effect">Año en curso</a></li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </li>
-    <li class="no-padding">
-        <ul class="collapsible collapsible-accordion">
-            <li>
-                <a class="collapsible-header waves-effect principal-item">Personal<i
-                        class="material-icons">assignment_ind</i></a>
-                <div class="collapsible-body">
-                    <ul>
-                        <li><a href="cargo.php" class="waves-effect">Cargos</a></li>
-                        <li><a href="empleado.php" class="waves-effect">Empleados</a></li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </li>
-
-    <li class="no-padding">
-        <ul class="collapsible collapsible-accordion">
-            <li>
-                <a class="collapsible-header waves-effect principal-item">Alumnos<i
-                        class="material-icons">school</i></a>
-                <div class="collapsible-body">
-                    <ul>
-                        <li><a href="alumno.php" class="waves-effect">Nuevo Alumno</a></li>
-                        <li><a href="detalle_alumno.php" class="waves-effect">Detalle de Alumno</a></li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </li>
-    <li class="no-padding">
-        <ul class="collapsible collapsible-accordion">
-            <li>
-                <a class="collapsible-header waves-effect principal-item">Materias<i
-                        class="material-icons">library_books</i></a>
-                <div class="collapsible-body">
-                    <ul>
-                        <li><a href="detalle_materia.php" class="waves-effect">Detalle Materias</a></li>                        
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </li>
+    <ul class="collapsible">
+        <li>
+            <div class="collapsible-header"><i class="material-icons">settings</i>Datos Generales</div>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="departamento.php" class="waves-effect">Departamentos</a></li>
+                    <li><a href="municipio.php" class="waves-effect">Municipios</a></li>
+                    <li><a href="school.php" class="waves-effect">Centros Educativos</a></li>
+                    <li><a href="class.php" class="waves-effect">Classs</a></li>
+                    <li><a href="seccion.php" class="waves-effect">Seccion</a></li>                    
+                    <li><a href="materia.php" class="waves-effect">Materias</a></li>
+                    <li><a href="#!" class="waves-effect">Año en curso</a></li>
+                </ul>
+            </div>
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">assignment_ind</i>Personal</div>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="cargo.php" class="waves-effect">Cargos</a></li>
+                    <li><a href="empleado.php" class="waves-effect">Empleados</a></li>
+                </ul>
+            </div>
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">school</i>Alumnos</div>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="alumno.php" class="waves-effect">Nuevo Alumno</a></li>
+                    <li><a href="detalle_alumno.php" class="waves-effect">Detalle de Alumno</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
 </ul>
 <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
